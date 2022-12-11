3840ed47324b85b408d7fa3312523dc0058d2e36
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-11 22:55:08 +0100

    unauthenticated users, login-required functionalities

--- project_tc_gen/urls.py
+++ project_tc_gen/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign-in/', views.signIn, name='sign-in'),
-    path('logout-confirm', views.logoutConfirm, name='logout-confirm'),
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
--- project_tc_gen/views.py
+++ project_tc_gen/views.py
@@ -3,11 +3,16 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
 from users.models import Profile
+
+from .decorators import unauthenticated_user
+
 
 def home(request):
     return render(request, 'project_tc_gen/index.html')
 
+@unauthenticated_user
 def signIn(request):
     if request.method=="POST":
         username = request.POST.get('username')
@@ -28,8 +33,6 @@ def signIn(request):
 
     return render(request, 'project_tc_gen/sign-in.html')
 
-def logoutConfirm(request):
-    return render(request, 'project_tc_gen/logout.html')
 
 def logout(request):
     if request.method== 'POST':
@@ -37,16 +40,20 @@ def logout(request):
         return redirect('sign-in')
     return render(request, 'project_tc_gen/logout.html')
 
+@login_required(login_url='sign-in')
 def dashboard(request):
     print(request.user.id)
     return render(request, 'project_tc_gen/dashboard.html')
 
+@login_required(login_url='sign-in')
 def faq(request):
     return render(request, 'project_tc_gen/faq.html')
 
+@login_required(login_url='sign-in')
 def about(request):
     return render(request, 'project_tc_gen/about.html')
 
+@login_required(login_url='sign-in')
 def profile(request):
     return render(request, 'project_tc_gen/profile.html')
 # Create your views here.
