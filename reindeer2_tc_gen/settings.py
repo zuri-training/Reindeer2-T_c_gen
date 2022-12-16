5d588353a2515e8a74b1fce193d04681b49a30bb
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-16 19:36:21 +0100

    Update settings.py

--- reindeer2_tc_gen/settings.py
+++ reindeer2_tc_gen/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +128,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath('static')
+]
+
+STATIC_ROOT = 'var/www/tohmi02/static'
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
