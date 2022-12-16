594ff3e23e1df8f0734ca618452620212679c354
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-16 22:00:46 +0100

    Update urls.py

--- reindeer2_tc_gen/urls.py
+++ reindeer2_tc_gen/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import serve
+from django.conf import settings
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
