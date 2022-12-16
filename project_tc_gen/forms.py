f98017f2bf1a6554b2c317ec811e8612829224a5
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-16 14:13:05 +0100

    Add files via upload

---
+++ project_tc_gen/forms.py
@@ -0,0 +1,25 @@
+from django.forms import ModelForm, PasswordInput
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import *
+
+from project_tc_gen.models import Document, Company
+from users.models import Profile 
+from django.contrib.auth.models import User
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        widgets = {
+            'password': PasswordInput(attrs={
+                'type': 'password',
+                'style': "width: 50%; height: 50%; font-family: 'Open Sans';font-style: normal;font-weight: 400;font-size: 20px;line-height: 27px;letter-spacing: 0.02em;color: #121212;flex: none;order: 0;flex-grow: 0;",
+                'placeholder': "Enter a Password",
+        }),
+        }
+        fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
+
+class GenerateDocument(ModelForm):
+    class Meta:
+        model = Document
+        fields = ['document_type']
+
---
+++ project_tc_gen/views.py
@@ -0,0 +1,128 @@
+
+# from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+
+from .models import Document
+from users.models import Profile
+
+from .forms import CreateUserForm, GenerateDocument
+
+from .decorators import unauthenticated_user
+
+
+def home(request):
+    user = request.user
+    context = {'user':'user'}
+    return render(request, 'project_tc_gen/index.html', context)
+
+def privacyPolicy(request):
+    return render(request, 'project_tc_gen/privacy-policy.html')
+
+def termsAndConditions(request):
+    return render(request, 'project_tc_gen/t&c.html')
+
+@login_required(login_url='sign-in')
+def faq(request):
+    return render(request, 'project_tc_gen/faq.html')
+
+@login_required(login_url='sign-in')
+def about(request):
+    return render(request, 'project_tc_gen/about.html')
+
+def contactUs(request):
+    return render(request, 'project_tc_gen/contact-us.html')
+
+def signUp(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username = form.cleaned_data.get('username')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+            # username = form.cleaned_data.get('username')
+            Profile.objects.create(user=user, username=user.username, first_name=user.first_name, last_name=user.last_name, email=user.email)
+            messages.success(request, f'Account was created for {username}')
+            return redirect('sign-in')
+    context = {'form': form} 
+    return render(request, 'project_tc_gen/sign-up.html', context)
+
+@unauthenticated_user
+def signIn(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            print(request.user.id)
+            return redirect("dashboard")
+        else:
+            messages.info(request, 'invalid credentials')
+    #         return redirect('sign-in')
+
+    # else:
+    #     return render(request, 'project_tc_gen/sign-in.html')
+
+    return render(request, 'project_tc_gen/sign-in.html')
+
+
+def logout(request):
+    if request.method== 'POST':
+        auth_logout(request)
+        return redirect('sign-in')
+    return render(request, 'project_tc_gen/logout.html')
+
+@login_required(login_url='sign-in')
+def dashboard(request):
+    context = {'first_name': request.user.first_name}
+    print(request.user.first_name)
+    return render(request, 'project_tc_gen/dashboard.html', context)
+
+@login_required(login_url='sign-in')
+def profile(request):
+    return render(request, 'project_tc_gen/profile.html')
+
+def TCtemplates(request):
+    return render(request, 'project_tc_gen/TandC TEMPLATE.html')
+
+def verification(request):
+    return render(request, 'project_tc_gen/verification-page.html')
+
+def projects(request):
+    form = GenerateDocument()
+    obj = Document.objects
+    context = {'form': form}
+    ms=['Terms and Conditions', 'Privacy Policy']
+    if request.method=='POST':
+        generator = request.POST.getlist('generator')
+        if generator==['Terms and Conditions']:
+            print("terms and conditions")
+            return render(request, 'project_tc_gen/generatedt&c.html')
+        if generator==['Privacy Policy']:
+            print("privacy policy")
+            return render(request, 'project_tc_gen/generatedprivacypolicy.html')
+        # form = GenerateDocument(request.POST)
+        # if form.is_valid():
+        #     form.save()
+            # return redirect("/")
+            # return render(request, 'project_tc_gen/privacy-policy.html')
+        # elif form.is_termsandcondition==True:
+        #     return render(request, 'project_tc_gen/t&c.html')
+    return render(request, 'project_tc_gen/projects.html', context)
+
+def error_404(request, pk):
+    pk=pk
+    context={'pk':pk}
+    return render(request, 'project_tc_gen/error-page.html', context)
+
+# Create your views here.
