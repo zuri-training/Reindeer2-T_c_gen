289d607d07cb6169af0fc7bb2c0a2274f9f40f11
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-17 21:38:21 +0100

    settings update

--- reindeer2_tc_gen/settings.py
+++ reindeer2_tc_gen/settings.py
@@ -102,7 +102,9 @@ DATABASES = {
     # }
 }
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
