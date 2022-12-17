4c4f6aeae991e5375a499c5c2d53d324dde445f8
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-12-17 22:23:37 +0100

    another fix deploy

--- reindeer2_tc_gen/settings.py
+++ reindeer2_tc_gen/settings.py
@@ -106,6 +106,7 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 # DATABASES['default'] = dj_database_url.config()
 
+# AUTH_USER_MODEL = 'users.Profile'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
